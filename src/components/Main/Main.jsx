2b2f20527ec7de7436e36ecf36c51884a3f58b61
galinafrost <morozgalina13@gmail.com>
2022-11-24 17:50:30 +0100

    hero mobile

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,17 @@
 import Form from "../Form";
 import LoginForm from "../LoginForm";
 
+import styles from "./main.module.scss";
+
 const Main = () => {
   return (
     <main>
-      <h1>My Wallet</h1>
-      <p>Smart Finance</p>
-      <LoginForm />
-      <Form />
+      <div className={styles.root}>
+        <h1 className={styles.title}>My Wallet</h1>
+        <p className={styles.text}>Smart Finance</p>
+        <LoginForm />
+        <Form />
+      </div>
     </main>
   );
 };
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,7 +1,5 @@
 import { useSelector } from "react-redux";
 
-// import { isLogin } from "../../redux/auth/auth-selectors";
-
 export const useAuth = () => {
   const isLogin = useSelector((state) => state.logIn.isLogin);
 
