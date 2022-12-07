e902594c7175f5d792e59de5c7413b34b0509f05
galinafrost <morozgalina13@gmail.com>
2022-12-07 18:01:54 +0100

    btn Google

--- src/components/ButtonGoogle/ButtonGoogle.jsx
+++ src/components/ButtonGoogle/ButtonGoogle.jsx
@@ -1,8 +1,11 @@
+import GoogleLogo from "../../img/btn-google.svg";
+
 import styles from "./buttongoogle.module.scss";
 
 const ButtonGoogle = () => {
   return (
     <button className={styles.btngoogle} type="button">
+      <img className={styles.logoGoogle} src={GoogleLogo} alt="Google logo" />
       Google
     </button>
   );
